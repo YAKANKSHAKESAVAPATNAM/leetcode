810f87cea2aa5d9424158f85937330d38540ed4d
Yakanksha K <147792127+YAKANKSHAKESAVAPATNAM@users.noreply.github.com>
2025-10-02 22:50:50 +0530

    Time: 4 ms (74.89%) | Memory: 10.9 MB (92.98%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,7 @@ public:
         int maxLength = 0;
         vector<int> charIndex(128, -1);
         int left = 0;
-        
+
         for (int right = 0; right < n; right++) {
             if (charIndex[s[right]] >= left) {
                 left = charIndex[s[right]] + 1;
@@ -13,7 +13,7 @@ public:
             charIndex[s[right]] = right;
             maxLength = max(maxLength, right - left + 1);
         }
-        
+
         return maxLength;
     }
 };
